b7eafb9d0ef2868eb0cdd5a327bfae4f22dcf915
Djessa <s.djessa.2000@gmail.com>
2022-10-12 17:31:13 +0300

    create post request to send file information

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const fileUpload = require('express-fileupload');
 var cors = require('cors');
 require('dotenv').config()
 
@@ -6,9 +7,15 @@ var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(fileUpload());
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', async (req, res, next) => {
+    const upfile = req.files.upfile;
+    return res.json({name: upfile.name, type: upfile.mimetype, size: upfile.size});
 });
 
 
